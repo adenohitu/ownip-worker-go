c4e0e1f870e066690c880b12a07bad755da62337
adenohitu <49752462+adenohitu@users.noreply.github.com>
2024-10-12 10:01:38 +0900

    init: go ownip tool

---
+++ main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/openrdap/rdap"
+)
+
+func main() {
+	r := gin.Default()
+	r.GET("/", func(ctx *gin.Context) {
+		client := &rdap.Client{}
+		domain, err := client.QueryIP(ctx.ClientIP())
+
+		if err == nil {
+			fmt.Printf("Handle=%s\n", domain.Handle)
+		}
+		fmt.Println(domain)
+		var Organization string = "----"
+
+		for _, v := range domain.Remarks {
+			if v.Title == "description" {
+				Organization = v.Description[0]
+			}
+		}
+		ctx.JSON(http.StatusOK, gin.H{
+			"ClientIP":     ctx.ClientIP(),
+			"Name":         domain.Name,
+			"Organization": Organization,
+		})
+	})
+	r.Run()
+}
