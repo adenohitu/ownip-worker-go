12b481eb44c327aad9340d474cb9ca9d1ef6f81f
adenohitu <49752462+adenohitu@users.noreply.github.com>
2024-10-16 21:34:44 +0900

    add: Check private address

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"net"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -11,36 +12,45 @@ import (
 func setupRouter() *gin.Engine {
 	r := gin.Default()
 	r.GET("/", func(ctx *gin.Context) {
-		client := &rdap.Client{}
-		domain, err := client.QueryIP(ctx.ClientIP())
-
-		if err == nil {
-			fmt.Printf("Handle=%s\n", domain.Handle)
-
-			var Organization string = ""
-
-			for _, v := range domain.Remarks {
-				if v.Title == "description" {
-					Organization = v.Description[0]
-				}
-			}
-
+		ip := net.ParseIP(ctx.ClientIP())
+		if ip.IsPrivate() {
 			ctx.JSON(http.StatusOK, gin.H{
 				"status":       "ok",
 				"ClientIP":     ctx.ClientIP(),
-				"Name":         domain.Name,
-				"Organization": Organization,
-			})
-
-		} else {
-			ctx.JSON(http.StatusOK, gin.H{
-				"status":       "rdapError",
-				"ClientIP":     ctx.ClientIP(),
-				"Name":         "",
+				"Name":         "internal",
 				"Organization": "",
 			})
-		}
+		} else {
+			client := &rdap.Client{}
+			domain, err := client.QueryIP(ctx.ClientIP())
 
+			if err == nil {
+				fmt.Printf("Handle=%s\n", domain.Handle)
+
+				var Organization string = ""
+
+				for _, v := range domain.Remarks {
+					if v.Title == "description" {
+						Organization = v.Description[0]
+					}
+				}
+
+				ctx.JSON(http.StatusOK, gin.H{
+					"status":       "ok",
+					"ClientIP":     ctx.ClientIP(),
+					"Name":         domain.Name,
+					"Organization": Organization,
+				})
+
+			} else {
+				ctx.JSON(http.StatusOK, gin.H{
+					"status":       "rdapError",
+					"ClientIP":     ctx.ClientIP(),
+					"Name":         "",
+					"Organization": "",
+				})
+			}
+		}
 	})
 
 	r.GET("/rdap/all", func(ctx *gin.Context) {
